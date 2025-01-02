17b1560cf5f126c44d7de5cf030558c0ba20e2be
NelliEfr <n.efremyan@gmail.com>
2025-01-02 13:53:08 +0400

    shopping cart localstorage added

--- src/app/stores/shoppingCart.ts
+++ src/app/stores/shoppingCart.ts
@@ -1,10 +1,37 @@
 import { defineStore } from "pinia";
-import { computed, ref } from "vue";
+import { computed, ref, watch } from "vue";
 import { type Product } from "@/entities/product/model/types";
 import { type CartProduct } from "@/entities/cart/model/types";
 
+const LOCAL_STORAGE_KEY = "shoppingCart";
+
 export const useShoppingCartStore = defineStore("shoppingCart", () => {
   const cartProducts = ref<CartProduct[]>([]);
+
+  const loadCartFromLocalStorage = () => {
+    const savedCart = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (savedCart) {
+      try {
+        cartProducts.value = JSON.parse(savedCart);
+      } catch (e) {
+        console.error("Failed to parse cart data from localStorage", e);
+      }
+    }
+  };
+
+  loadCartFromLocalStorage();
+
+  const saveCartToLocalStorage = () => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(cartProducts.value));
+  };
+
+  watch(
+    cartProducts,
+    () => {
+      saveCartToLocalStorage();
+    },
+    { deep: true }
+  );
 
   const addProduct = (product: Product) => {
     const existingProduct = cartProducts.value.find(el => el.id === product.id);
